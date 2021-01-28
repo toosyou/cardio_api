834318c216a45dab382d6147801a83a67cbb607a
toosyou <toosyou.tw@gmail.com>
2021-01-28 15:11:01 +0800

    fix install script

--- install.sh
+++ install.sh
@@ -8,9 +8,9 @@ export LANG=C.UTF-8
 git clone --recurse-submodules https://github.com/toosyou/ekg abnormal
 
 PIPENV_PIPFILE=/abnormal/Pipfile pipenv sync
-PIPENV_PIPFILE=/abnormal/Pipfile NVIDIA_VISIBLE_DEVICES="1"\ 
-                                    pipenv run python3 /abnormal/web/api/apiv2.py\ 
-                                     -e toosyou/ekg-abnormal_detection/mhr2ocd1\ 
-                                     -p toosyou/ekg-abnormal_detection/2ojtwj7c\ 
-                                     -w toosyou/ekg-abnormal_detection/x65zqmoy\ 
+PIPENV_PIPFILE=/abnormal/Pipfile NVIDIA_VISIBLE_DEVICES="1"\
+                                    pipenv run python3 /abnormal/web/api/apiv2.py\
+                                     -e toosyou/ekg-abnormal_detection/mhr2ocd1\
+                                     -p toosyou/ekg-abnormal_detection/2ojtwj7c\
+                                     -w toosyou/ekg-abnormal_detection/x65zqmoy\
                                      -n 5 & # port 8787
