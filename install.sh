fda8a695900c691d334540d6614c0b5dcdd31500
toosyou <toosyou.tw@gmail.com>
2021-01-28 15:30:20 +0800

    tiny fix

--- install.sh
+++ install.sh
@@ -8,7 +8,7 @@ export LANG=C.UTF-8
 git clone --recurse-submodules https://github.com/toosyou/ekg abnormal
 
 PIPENV_PIPFILE=/abnormal/Pipfile pipenv sync
-PIPENV_PIPFILE=/abnormal/Pipfile pipenv wandb login --anonymously
+PIPENV_PIPFILE=/abnormal/Pipfile pipenv run wandb login --anonymously
 PIPENV_PIPFILE=/abnormal/Pipfile NVIDIA_VISIBLE_DEVICES="1" \
                                     pipenv run python3 /abnormal/web/api/apiv2.py \
                                      -e toosyou/ekg-abnormal_detection/mhr2ocd1 \
