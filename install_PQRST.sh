1bcb9f54d963d135ac3fdd0a632cd8c126272b45
toosyou <toosyou.tw@gmail.com>
2021-02-01 17:28:40 +0800

    fix pqrst path issue

--- install_PQRST.sh
+++ install_PQRST.sh
@@ -7,9 +7,10 @@ export LANG=C.UTF-8
 # download PQRST segmentation repo
 git clone --recurse-submodules https://github.com/writerwriter/PQRST-Segmentation PQRST
 
-wget -O /PQRST/weight.pkl https://nas.miplab.org:5001/sharing/EaMNy6NC2
+cd /PQRST
+wget -O weight.pkl https://nas.miplab.org:5001/sharing/EaMNy6NC2
 
-PIPENV_PIPFILE=/PQRST/Pipfile pipenv sync
-PIPENV_PIPFILE=/PQRST/Pipfile NVIDIA_VISIBLE_DEVICES="0" \
-                                pipenv run python3 /PQRST/server.py \
-                                 -p /PQRST/weight.pkl #port 8899
+pipenv sync
+NVIDIA_VISIBLE_DEVICES="0" \
+    pipenv run python3 server.py \
+        -p weight.pkl #port 8899
